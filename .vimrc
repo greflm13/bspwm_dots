22cf2ee5b2a7a4c94a326fc49e061957686a535b
greistof <florian.greistorfer@acp.at>
2020-07-31 15:34:56 +0200

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,33 @@
+" ===== plugins list =====
+call plug#begin('~/.vim/plugged')
+
+Plug 'sheerun/vim-polyglot'
+Plug 'greflm13/monokai-vibrant'
+Plug 'itchyny/lightline.vim'
+
+call plug#end()
+
+" ===== vim builtin config =====
+syntax enable
+set termguicolors
+set showtabline=2
+set laststatus=2
+set number
+set noshowmode
+
+" ===== colorscheme config =====
+colorscheme monokai_vibrant
+
+" ===== lightline config =====
+let g:lightline = {
+    \ 'colorscheme': 'monokai_vibrant',
+    \ 'active': {
+    \ 'left': [ ['mode'], ['filename'] ],
+    \ 'right': [ ['filetype'] ]},
+    \ 'inactive': {
+    \ 'left': [ ['filename'] ],
+    \ 'right': [] }
+    \ }
+let g:lightline.tab = {
+ \ 'active' : [ 'filename', 'modified' ],
+ \ 'inactive' : [ 'filename', 'modified' ] }
---
+++ config/bspwm/bspwm_workspaces.sh
@@ -0,0 +1,19 @@
+bspc subscribe monitor | while read -r line; do
+  case $line in
+      monitor_add*|monitor_geometry*)
+	autorandr -c
+        if [ "$(xrandr --query | grep '\bconnected' | wc -l)" -eq "3" ]; then
+          bspc monitor $(xrandr --query | grep '\bconnected' | sed -n 1p | sed 's/ .*//') -d 1 2 3
+          bspc monitor $(xrandr --query | grep '\bconnected' | sed -n 2p | sed 's/ .*//') -d 4 5 6
+          bspc monitor $(xrandr --query | grep '\bconnected' | sed -n 3p | sed 's/ .*//') -d 7 8 9
+        elif [ "$(xrandr --query | grep '\bconnected' | wc -l)" -eq "2" ]; then
+          bspc monitor $(xrandr --query | grep '\bconnected' | sed -n 1p | sed 's/ .*//') -d 1 2 3 4 5
+          bspc monitor $(xrandr --query | grep '\bconnected' | sed -n 2p | sed 's/ .*//') -d 6 7 8 9
+        else
+          bspc monitor -d 1 2 3 4 5 6 7 8 9
+        fi
+        ;;
+      *)
+      ;;
+  esac
+done 
