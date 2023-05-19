99807026f67969fba5f01410c891e452de819f43
Flo Greistorfer <florian.greistorfer@k-business.at>
2023-05-19 13:31:43 +0200

    Change color switch script

--- config/polybar/scripts/color-switch.sh
+++ config/polybar/scripts/color-switch.sh
@@ -1,50 +1,75 @@
 #!/bin/bash
 
-## Author : Aditya Shakya
-## Github : adi1090x
+echo -e "\x1b[48;2;20;21;26m                                          \x1b[0m"
+echo -e "\x1b[48;2;20;21;26m   7      8      9     10     11     12   \e[m"
+echo -e "\e[48;5;9m       \e[48;5;10m       \e[48;5;11m       \e[48;5;12m       \e[48;5;13m       \e[48;5;14m       \e[m"
+echo -e "\x1b[48;2;20;21;26m                                          \x1b[0m"
+echo -e "\x1b[48;2;20;21;26m  13     14     15     16     17     18   \e[m"
+echo -e "\x1b[48;2;140;12;0m       \x1b[48;2;137;175;109m       \x1b[48;2;206;148;31m       \x1b[48;2;59;76;111m       \x1b[48;2;178;108;199m       \x1b[48;2;77;164;175m       \x1b[0m"
+echo ""
+color=""
+while [[ $color == "" ]]; do
+    read -r choice
+    case $choice in
+    "1")
+        color=e53947
+        ;;
+    "2")
+        color=74e000
+        ;;
+    "3")
+        color=e5c33e
+        ;;
+    "4")
+        color=4a7de5
+        ;;
+    "5")
+        color=ce3be5
+        ;;
+    "6")
+        color=16bcce
+        ;;
+    "7")
+        color=af3a32
+        ;;
+    "8")
+        color=64c20c
+        ;;
+    "9")
+        color=e5d083
+        ;;
+    "10")
+        color=579dd7
+        ;;
+    "11")
+        color=cc67b9
+        ;;
+    "12")
+        color=5fd3de
+        ;;
+    "13")
+        color=7e0b00
+        ;;
+    "14")
+        color=7b9d62
+        ;;
+    "15")
+        color=b9851c
+        ;;
+    "16")
+        color=354464
+        ;;
+    "17")
+        color=a061b3
+        ;;
+    "18")
+        color=45949d
+        ;;
+    *) ;;
 
-SDIR="$HOME/.config/polybar/scripts"
-
-MENU="$(rofi -sep "|" -dmenu -i -p 'Select' -location 5 -columns 1 -xoffset -240 -yoffset -52 -width 12 -hide-scrollbar -line-padding 4 -padding 20 -lines 10 <<< " amber| blue| blue-grey| brown| cyan| deep-orange| deep-purple| green| grey| indigo| light-blue| light-green| lime| orange| pink| purple| red| teal| yellow| amber-dark| blue-dark| blue-grey-dark| brown-dark| cyan-dark| deep-orange-dark| deep-purple-dark| green-dark| grey-dark| indigo-dark| light-blue-dark| light-green-dark| lime-dark| orange-dark| pink-dark| purple-dark| red-dark| teal-dark| yellow-dark")"
-case "$MENU" in
-    ## Light Colors
-    *amber) $SDIR/colors-light.sh -amber ;;
-    *blue) $SDIR/colors-light.sh -blue ;;
-    *blue-grey) $SDIR/colors-light.sh -blue-grey ;;
-    *brown) $SDIR/colors-light.sh -brown ;;
-    *cyan) $SDIR/colors-light.sh -cyan ;;
-    *deep-orange) $SDIR/colors-light.sh -deep-orange ;;
-    *deep-purple) $SDIR/colors-light.sh -deep-purple ;;
-    *green) $SDIR/colors-light.sh -green ;;
-    *grey) $SDIR/colors-light.sh -grey ;;
-    *indigo) $SDIR/colors-light.sh -indigo ;;
-    *light-blue) $SDIR/colors-light.sh -light-blue ;;
-    *light-green) $SDIR/colors-light.sh -light-green ;;
-    *lime) $SDIR/colors-light.sh -lime ;;
-    *orange) $SDIR/colors-light.sh -orange ;;
-    *pink) $SDIR/colors-light.sh -pink ;;
-    *purple) $SDIR/colors-light.sh -purple ;;
-    *red) $SDIR/colors-light.sh -red ;;
-    *teal) $SDIR/colors-light.sh -teal ;;
-    *yellow) $SDIR/colors-light.sh -yellow ;;
-    ## Dark Colors
-    *amber-dark) $SDIR/colors-dark.sh -amber ;;
-    *blue-dark) $SDIR/colors-dark.sh -blue ;;
-    *blue-grey-dark) $SDIR/colors-dark.sh -blue-grey ;;
-    *brown-dark) $SDIR/colors-dark.sh -brown ;;
-    *cyan-dark) $SDIR/colors-dark.sh -cyan ;;
-    *deep-orange-dark) $SDIR/colors-dark.sh -deep-orange ;;
-    *deep-purple-dark) $SDIR/colors-dark.sh -deep-purple ;;
-    *green-dark) $SDIR/colors-dark.sh -green ;;
-    *grey-dark) $SDIR/colors-dark.sh -grey ;;
-    *indigo-dark) $SDIR/colors-dark.sh -indigo ;;
-    *light-blue-dark) $SDIR/colors-dark.sh -light-blue ;;
-    *light-green-dark) $SDIR/colors-dark.sh -light-green ;;
-    *lime-dark) $SDIR/colors-dark.sh -lime ;;
-    *orange-dark) $SDIR/colors-dark.sh -orange ;;
-    *pink-dark) $SDIR/colors-dark.sh -pink ;;
-    *purple-dark) $SDIR/colors-dark.sh -purple ;;
-    *red-dark) $SDIR/colors-dark.sh -red ;;
-    *teal-dark) $SDIR/colors-dark.sh -teal ;;
-    *yellow-dark) $SDIR/colors-dark.sh -yellow
-esac
+    esac
+done
+sed -i "s/^ac\s.*/ac = #${color}/g" "${HOME}"/.config/polybar/colors.ini
+sed -i "s/^drawColor=.*/drawColor=#${color}/g" "${HOME}"/.config/flameshot/flameshot.ini
+sed -i "s/^uiColor=.*/uiColor=#${color}/g" "${HOME}"/.config/flameshot/flameshot.ini
+sed -i "s/^\s*frame_color = .*/    frame_color = \"#${color}\"/g" "${HOME}"/.config/dunst/dunstrc
