aee500d36cf531490ee8ce7156b3cb6947f4eaed
Flo Greistorfer <florian.greistorfer@k-business.com>
2023-05-25 12:56:06 +0200

    changed pactl to wpctl and removed no longer neccessary scripts

--- config/sxhkd/volume.sh
+++ config/sxhkd/volume.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 MENU="$(echo "" | dmenu -p "Volume" -nb '#08090c' -nf '#f6f6f6' -sb '#08090c' -sf '#f6f6f6')"
-pactl set-sink-volume @DEFAULT_SINK@ "$MENU"%
+wpctl set-volume @DEFAULT_AUDIO_SINK@ "$MENU"%
