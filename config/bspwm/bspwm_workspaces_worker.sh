4edbf30bf6f15c78dc29221b9e717cf87c715bb7
Flo Greistorfer <florian.greistorfer@k-business.com>
2024-01-11 20:06:49 +0100

    changed loop items

--- config/bspwm/bspwm_workspaces_worker.sh
+++ config/bspwm/bspwm_workspaces_worker.sh
@@ -8,16 +8,16 @@ if [ "$(<~/.config/bspwm/lock)" != "lock" ]; then
         for MONITOR in $(xrandr --query | grep '\bconnected' | sed 's/ .*//'); do
             bspc monitor "$MONITOR" -a Desktop
         done
-        for DESKTOP in 1 2 3; do
+        for DESKTOP in {1..3}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '1p')"+ | awk '{print $1}')"
         done
-        for DESKTOP in 4 5 6; do
+        for DESKTOP in {4..6}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '2p')"+ | awk '{print $1}')"
         done
-        for DESKTOP in 7 8 9; do
+        for DESKTOP in {7..9}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '3p')"+ | awk '{print $1}')"
         done
-        for DESKTOP in 10 11 12; do
+        for DESKTOP in {10..12}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '4p')"+ | awk '{print $1}')"
         done
         for MONITOR in $(xrandr --query | grep disconnected | sed 's/ .*//'); do
@@ -33,13 +33,13 @@ if [ "$(<~/.config/bspwm/lock)" != "lock" ]; then
         for MONITOR in $(xrandr --query | grep '\bconnected' | sed 's/ .*//'); do
             bspc monitor "$MONITOR" -a Desktop
         done
-        for DESKTOP in 1 2 3 4; do
+        for DESKTOP in {1..4}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '1p')"+ | awk '{print $1}')"
         done
-        for DESKTOP in 5 6 7 8; do
+        for DESKTOP in {5..8}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '2p')"+ | awk '{print $1}')"
         done
-        for DESKTOP in 9 10 11 12; do
+        for DESKTOP in {9..12}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '3p')"+ | awk '{print $1}')"
         done
         for MONITOR in $(xrandr --query | grep disconnected | sed 's/ .*//'); do
@@ -55,10 +55,10 @@ if [ "$(<~/.config/bspwm/lock)" != "lock" ]; then
         for MONITOR in $(xrandr --query | grep '\bconnected' | sed 's/ .*//'); do
             bspc monitor "$MONITOR" -a Desktop
         done
-        for DESKTOP in 1 2 3 4 5 6; do
+        for DESKTOP in {1..6}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '1p')"+ | awk '{print $1}')"
         done
-        for DESKTOP in 7 8 9 10 11 12; do
+        for DESKTOP in {7..12}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '2p')"+ | awk '{print $1}')"
         done
         for MONITOR in $(xrandr --query | grep disconnected | sed 's/ .*//'); do
@@ -74,7 +74,7 @@ if [ "$(<~/.config/bspwm/lock)" != "lock" ]; then
         for MONITOR in $(xrandr --query | grep '\bconnected' | sed 's/ .*//'); do
             bspc monitor "$MONITOR" -a Desktop
         done
-        for DESKTOP in 1 2 3 4 5 6 7 8 9 10 11 12; do
+        for DESKTOP in {1..12}; do
             bspc desktop $DESKTOP -m "$(xrandr -q | grep '\bconnected' | grep +"$(echo "$sorted" | sed -n '1p')"+ | awk '{print $1}')"
         done
         for MONITOR in $(xrandr --query | grep disconnected | sed 's/ .*//'); do
