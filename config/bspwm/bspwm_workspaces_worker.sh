f9f0d94f59b8450a2f19f702cc219c729bfe8016
Flo Greistorfer <fgreistorfer@anexia.com>
2025-04-01 18:20:49 +0200

    fixed some issues

--- config/bspwm/bspwm_workspaces_worker.sh
+++ config/bspwm/bspwm_workspaces_worker.sh
@@ -2,10 +2,10 @@
 autorandr -c
 if [ "$(<~/.config/bspwm/lock)" != "lock" ]; then
     echo "lock" >~/.config/bspwm/lock
-    sorted=$(xrandr -q | grep -E '\bconnected [[:digit:]]{1,4}x' | sed 's/primary//' | awk '{print $3}' | sed 's/.*+\(.*\)+.*/\1/' | sort)
-    inactive=$(xrandr -q | grep -E '\bconnected ([[:alpha:]]+|\()' | sed 's/primary//' | awk '{print $1}' | sort)
-    num="$(xrandr --query | grep -cE '\bconnected [[:digit:]]{1,4}x')"
-    for MONITOR in $(xrandr --query | grep -E '\bconnected [[:digit:]]{1,4}x' | sed 's/ .*//'); do
+    sorted=$(xrandr -q | grep -E '\bconnected( primary)? [[:digit:]]{1,4}x' | sed 's/primary//' | awk '{print $3}' | sed 's/.*+\(.*\)+.*/\1/' | sort)
+    inactive=$(xrandr -q | grep -E '\bconnected(!? primary) ([[:alpha:]]+|\()' | sed 's/primary//' | awk '{print $1}' | sort)
+    num="$(xrandr --query | grep -cE '\bconnected( primary)? [[:digit:]]{1,4}x')"
+    for MONITOR in $(xrandr --query | grep -E '\bconnected( primary)? [[:digit:]]{1,4}x' | sed 's/ .*//'); do
         bspc monitor "$MONITOR" -a Desktop
     done
     if [ "$num" -eq "4" ]; then
@@ -52,7 +52,7 @@ if [ "$(<~/.config/bspwm/lock)" != "lock" ]; then
     for DESKTOP in $(bspc query -D --names | grep Desktop); do
         bspc desktop Desktop -r
     done
-    for MONITOR in $(xrandr --query | grep -E '\bconnected [[:digit:]]{1,4}x' | sed 's/ .*//'); do
+    for MONITOR in $(xrandr --query | grep -E '\bconnected( primary)? [[:digit:]]{1,4}x' | sed 's/ .*//'); do
         bspc query -m "$MONITOR" -T | jq -r .desktops[].name | sort -n | xargs bspc monitor "$MONITOR" -o
     done
     ~/.config/bspwm/lockscreen &
